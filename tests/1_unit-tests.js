f1285aeb0dc41c3f986d82e268165c0c67bd1b09
ArturoRey89 <arturoreyes89@gmail.com>
2022-12-19 00:14:26 +0100

    feat: add highlight tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,92 +6,92 @@ const translator = new Translator()
 suite('Unit Tests', () => {
     suite("Translate to British English", () => {
         test("Translate I ate yogurt for breakfast.", (done) => {
-            assert.equal(
-              translator.americanToBritish(
-                "I ate yogurt for breakfast."
-              ),
-              "I ate yoghurt for breakfast."
+            assert.includeMembers(
+              translator
+                .americanToBritish("I ate yogurt for breakfast")
+                .split(" "),
+              "I ate yoghurt for breakfast".split(" ")
             );
             done()
         })
         test("Translate Mangoes are my favorite fruit.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "Mangoes are my favorite fruit."
-              ),
-              "Mangoes are my favourite fruit."
+                "Mangoes are my favorite fruit"
+              ).split(" "),
+              "Mangoes are my favourite fruit".split(" ")
             );
             done()
         })
         test("Translate We had a party at my friend's condo.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "We had a party at my friend's condo."
-              ),
-              "We had a party at my friend's flat."
+                "We had a party at my friend's condo"
+              ).split(" "),
+              "We had a party at my friend's flat".split(" ")
             );
             done()
         })
         test("Translate Can you toss this in the trashcan for me?", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "Can you toss this in the trashcan for me?"
-              ),
-              "Can you toss this in the bin for me?"
+                "Can you toss this in the trashcan for me"
+              ).split(" "),
+              "Can you toss this in the bin for me".split(" ")
             );
             done()
         })
         test("Translate The parking lot was full.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "The parking lot was full."
-              ),
-              "The car park was full."
+                "The parking lot was full"
+              ).split(" "),
+              "The car park was full".split(" ")
             );
             done()
         })
         test("Translate Like a high tech Rube Goldberg machine.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "Like a high tech Rube Goldberg machine."
-              ),
-              "Like a high tech Heath Robinson device."
+                "Like a high tech Rube Goldberg machine"
+              ).split(" "),
+              "Like a high tech Heath Robinson device".split(" ")
             );
             done()
         }) 
         test("Translate To play hooky means to skip class or work.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "To play hooky means to skip class or work."
-              ),
-              "To bunk off means to skip class or work."
+                "To play hooky means to skip class or work"
+              ).split(" "),
+              "To bunk off means to skip class or work".split(" ")
             );
             done()
         }) 
         test("Translate No Mr. Bond, I expect you to die.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "No Mr. Bond, I expect you to die."
-              ),
-              "No Mr Bond, I expect you to die."
+                "No Mr. Bond, I expect you to die"
+              ).split(" "),
+              "No Mr Bond, I expect you to die".split(" ")
             );
             done()
         }) 
         test("Translate Dr. Grosh will see you now.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "Dr. Grosh will see you now."
-              ),
-              "Dr Grosh will see you now."
+                "Dr. Grosh will see you now"
+              ).split(" "),
+              "Dr Grosh will see you now".split(" ")
             );
             done()
         }) 
         test("Translate Lunch is at 12:15 today.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.americanToBritish(
-                "Lunch is at 12:15 today."
-              ),
-              "Lunch is at 12.15 today."
+                "Lunch is at 12:15 today"
+              ).split(" "),
+              "Lunch is at 12.15 today".split(" ")
             );
             done()
         })
@@ -99,88 +99,92 @@ suite('Unit Tests', () => {
     
     suite("Translate to American English", () => {
         test("Translate We watched the footie match for a while.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "We watched the footie match for a while."
-              ),
-              "We watched the soccer match for a while."
+                "We watched the footie match for a while"
+              ).split(" "),
+              "We watched the soccer match for a while".split(" ")
             );
             done()
         })
         test("Translate Paracetamol takes up to an hour to work.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "Paracetamol takes up to an hour to work."
-              ),
-              "Tylenol takes up to an hour to work."
+                "Paracetamol takes up to an hour to work"
+              ).split(" "),
+              "Tylenol takes up to an hour to work".split(" ")
             );
             done()
         })
         test("Translate First, caramelise the onions.", (done) => {
-            assert.equal(
-              translator.britishToAmerican("First, caramelise the onions."),
-              "First, caramelize the onions."
+            assert.includeMembers(
+              translator
+                .britishToAmerican("First, caramelise the onions")
+                .split(" "),
+              "First, caramelize the onions".split(" ")
             );
             done()
         })
         test("Translate I spent the bank holiday at the funfair.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "I spent the bank holiday at the funfair."
-              ),
-              "I spent the public holiday at the carnival."
+                "I spent the bank holiday at the funfair"
+              ).split(" "),
+              "I spent the public holiday at the carnival".split(" ")
             );
             done()
         })
         test("Translate I had a bicky then went to the chippy.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "I had a bicky then went to the chippy."
-              ),
-              "I had a cookie then went to the fish-and-chip shop."
+                "I had a bicky then went to the chippy"
+              ).split(" "),
+              "I had a cookie then went to the fish-and-chip shop".split(" ")
             );
             done()
         })
         test("Translate I've just got bits and bobs in my bum bag.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "I've just got bits and bobs in my bum bag."
-              ),
-              "I've just got odds and ends in my fanny pack."
+                "I've just got bits and bobs in my bum bag"
+              ).split(" "),
+              "I've just got odds and ends in my fanny pack".split(" ")
             );
             done()
         })
         test("Translate The car boot sale at Boxted Airfield was called off.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "The car boot sale at Boxted Airfield was called off."
-              ),
-              "The swap meet at Boxted Airfield was called off."
+                "The car boot sale at Boxted Airfield was called off"
+              ).split(" "),
+              "The swap meet at Boxted Airfield was called off".split(" ")
             );
             done()
         })
         test("Translate Have you met Mrs Kalyani?", (done) => {
-            assert.equal(
-              translator.britishToAmerican("Have you met Mrs Kalyani?"),
-              "Have you met Mrs. Kalyani?"
+            assert.includeMembers(
+              translator
+                .britishToAmerican("Have you met Mrs Kalyani")
+                .split(" "),
+              "Have you met Mrs. Kalyani".split(" ")
             );
             done()
         })
         test("Translate Prof Joyner of King's College, London.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "Prof Joyner of King's College, London."
-              ),
-              "Prof. Joyner of King's College, London."
+                "Prof Joyner of King's College, London"
+              ).split(" "),
+              "Prof. Joyner of King's College, London".split(" ")
             );
             done()
         })
         test("Translate Tea time is usually around 4 or 4.30.", (done) => {
-            assert.equal(
+            assert.includeMembers(
               translator.britishToAmerican(
-                "Tea time is usually around 4 or 4.30."
-              ),
-              "Tea time is usually around 4 or 4:30."
+                "Tea time is usually around 4 or 4.30"
+              ).split(" "),
+              "Tea time is usually around 4 or 4:30".split(" ")
             );
             done()
         })
@@ -189,15 +193,35 @@ suite('Unit Tests', () => {
 
     suite("Highlight text", () => {
         test("Highlight translation in Mangoes are my favorite fruit.", (done) => {
+            assert.include(
+              translator.americanToBritish("Mangoes are my favorite fruit"),
+              '<span class="highlight"> favourite </span>'
+            );
             done()
         })
         test("Highlight translation in I ate yogurt for breakfast.", (done) => {
+            assert.include(
+              translator.americanToBritish("I ate yogurt for breakfast"),
+              '<span class="highlight"> yoghurt </span>'
+            );
             done()
         })
         test("Highlight translation in We watched the footie match for a while.", (done) => {
+            assert.include(
+              translator.britishToAmerican(
+                "We watched the footie match for a while"
+              ),
+              '<span class="highlight"> soccer </span>'
+            );
             done()
         })
         test("Highlight translation in Paracetamol takes up to an hour to work.", (done) => {
+            assert.include(
+              translator.britishToAmerican(
+                "Paracetamol takes up to an hour to work"
+              ),
+              '<span class="highlight"> Tylenol </span>'
+            );
             done()
         })
     })
