c7d1533a1e83a4d5413f35d62bf8c773150c76e3
ArturoRey89 <arturoreyes89@gmail.com>
2022-12-19 01:50:48 +0100

    fix: POST api/translate

--- routes/api.js
+++ routes/api.js
@@ -1,40 +1,52 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
-const translator = new Translator()
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      let inputText = req.body.text;
-      let locale = req.body.locale;
-      if (inputText == "") {
-        res.json({ error: "No text to translate" });
+  app.route("/api/translate").post((req, res) => {
+    let inputText = req.body.text;
+    let locale = req.body.locale;
+    if (typeof inputText === "undefined" || typeof locale === "undefined") {
+      res.json({ error: "Required field(s) missing" });
+      return;
+    }
+
+    if (inputText == "") {
+      res.json({ error: "No text to translate" });
+      return;
+    }
+
+    if (locale != "american-to-british" && locale != "british-to-american") {
+      res.json({ error: "Invalid value for locale field" });
+      return;
+    }
+
+    if (locale == "american-to-british") {
+      let britishText = translator.americanToBritish(inputText);
+      if (britishText == inputText) {
+        res.send({
+          text: inputText,
+          translation: "Everything looks good to me!",
+        });
         return;
       }
-      if (locale != "american-to-british" && locale != "british-to-american") {
-        res.json({ error: "Invalid value for locale field" });
+      res.send({ text: inputText, translation: britishText });
+      return;
+    }
+
+    if (locale == "british-to-american") {
+      let americanText = translator.americanToBritish(inputText);
+      if (americanText == inputText) {
+        res.send({
+          text: inputText,
+          translation: "Everything looks good to me!",
+        });
         return;
       }
-      if (locale == "american-to-british") {
-        let britishText = translator.americanToBritish(text);
-        if (britishText === inputText) {
-          res.send({ text: inputText, translation: "Everything looks good to me!" });
-          return;
-        }
-        res.send({ text: text, translation: britishText });
-        return;
-      }
-      if (locale == "british-to-american") {
-        let americanText = translator.americanToBritish(text);
-        if (americanText === inputText) {
-          res.send({ text: inputText, translation: "Everything looks good to me!" });
-          return;
-        }
-        res.send({ text: inputText, translation: americanText });
-        return;
-      }
-    });
+      res.send({ text: inputText, translation: americanText });
+      return;
+    }
+  });
 };
