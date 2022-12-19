704d5d8c7823bab3832b3a0c857d3f048458b5e4
ArturoRey89 <arturoreyes89@gmail.com>
2022-12-19 01:48:29 +0100

    feat: POST api/translate

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,40 @@
 'use strict';
 
 const Translator = require('../components/translator.js');
-
+const translator = new Translator()
 module.exports = function (app) {
   
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let inputText = req.body.text;
+      let locale = req.body.locale;
+      if (inputText == "") {
+        res.json({ error: "No text to translate" });
+        return;
+      }
+      if (locale != "american-to-british" && locale != "british-to-american") {
+        res.json({ error: "Invalid value for locale field" });
+        return;
+      }
+      if (locale == "american-to-british") {
+        let britishText = translator.americanToBritish(text);
+        if (britishText === inputText) {
+          res.send({ text: inputText, translation: "Everything looks good to me!" });
+          return;
+        }
+        res.send({ text: text, translation: britishText });
+        return;
+      }
+      if (locale == "british-to-american") {
+        let americanText = translator.americanToBritish(text);
+        if (americanText === inputText) {
+          res.send({ text: inputText, translation: "Everything looks good to me!" });
+          return;
+        }
+        res.send({ text: inputText, translation: americanText });
+        return;
+      }
     });
 };
